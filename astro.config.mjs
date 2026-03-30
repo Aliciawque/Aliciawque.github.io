62e7caaa912ed0c098beeae92aa7ca0c785290b1
May Lucia <malan@usf.edu>
2026-03-30 08:10:58 -0400

    fix: remove Astro i18n redirect, fix project cards layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,4 @@ import react from '@astrojs/react'
 export default defineConfig({
   site: 'https://aliciawque.github.io',
   integrations: [react()],
-  i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh', 'en'],
-    routing: {
-      prefixDefaultLocale: true,
-    },
-  },
 })
